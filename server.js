2967a59ccd297922f7825289023c632a33b69583
Shrey_Jain <shrey@coderwitz.com>
2020-01-30 12:15:05 +0530

    deploy app at heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist"));
 
 app.all("*", (req, res) => {
-  res.status(200).sendFile(__dirname + "/dist");
+  res.status(200).sendFile( ".angular deploy/dist/index.html");
 });
 
 app.listen(process.env.port || 8000)
